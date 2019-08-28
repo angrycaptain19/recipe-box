75cb50cefbdd3f64adbb873617972fe7e7a8fdd6
Tim Gilboy <timgilboy@tims-mbp.mynet>
2019-08-28 17:20:48 +0100

    fixed init

--- app/__init__.py
+++ app/__init__.py
@@ -29,6 +29,12 @@ def create_app(config_class=Config):
     bootstrap.init_app(app)
     moment.init_app(app)
 
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
     #Logging errors via email if mail server is set up
     if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
@@ -65,4 +71,4 @@ def create_app(config_class=Config):
 
     return app      
 
-from app import routes, models, errors
+from app import models
