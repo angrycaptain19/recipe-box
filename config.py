18ffa17a7a7b7fbfb6e08f8580fd2bbb6f03cbac
Tim Gilboy <timgilboy@dyn908-163.guest.ic.ac.uk>
2019-09-19 11:00:59 +0100

    updated postgres db url

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
 	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 	WTF_CSRF_SECRET_KEY='a secondary scret key'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_ONYX_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
 	#Setting up email config
