35b5a3e96ee23c8425bf7c614108958fb9b03215
Tim Gilboy <timgilboy@dyn3150-34.wlan.ic.ac.uk>
2019-09-19 11:05:54 +0100

    added a register link

--- app/main/routes.py
+++ app/main/routes.py
@@ -229,7 +229,7 @@ def register():
 		return redirect(url_for('main.index'))
 	form = RegistrationForm()
 	if form.validate_on_submit():
-		user = User(username=form.username.data, email=form.email.data, isAdmin=True)
+		user = User(username=form.username.data, email=form.email.data)
 		user.set_password(form.password.data)
 		db.session.add(user)
 		db.session.commit()
