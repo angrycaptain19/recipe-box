65131ffcc649fc6ff2860103a32bf90825ec586b
Tim Gilboy <timgilboy@tims-mbp.mynet>
2020-01-03 10:36:37 +0000

    trying to fix database issues

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ class Config(object):
 	#SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 	SECRET_KEY = 'you-will-never-guess'
 	WTF_CSRF_SECRET_KEY='a secondary scret key'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_WHITE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
